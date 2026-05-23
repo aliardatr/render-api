053bb37a629842fa7ae2b17ca0137072bfbacb33
Ali Arda <yilmazaliarda128@gmail.com>
2026-05-23 14:12:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import json
 # ==========================================
 # 1. VERİTABANI KURULUMU VE ŞEMALAR
 # ==========================================
-db_path = "/tmp/haberler.db" if os.getenv("RENDER") else "./haberler.db"
-DATABASE_URL = f"sqlite:///{db_path}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
